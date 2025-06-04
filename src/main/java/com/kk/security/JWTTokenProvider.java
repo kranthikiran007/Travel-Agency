dfec13e0a61c7476a76578a35c0208ddf2d4cd59
Gopinambala Kranthi Kiran <103349982+kranthikiran007@users.noreply.github.com>
2025-06-04 09:49:07 +0530

    Update JWTTokenProvider.java

--- src/main/java/com/kk/security/JWTTokenProvider.java
+++ src/main/java/com/kk/security/JWTTokenProvider.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Component
+@PropertySource("classpath:security.env") 
 public class JWTTokenProvider {
 
 	@Value("${api.jwt.key}")
